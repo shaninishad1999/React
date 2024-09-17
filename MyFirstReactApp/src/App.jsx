231eebba93721c44a51981e02a8c70b51a128fc9
shaninishad1999 <shaninishad1999@gmail.com>
2024-09-18 00:09:46 +0530

    connect component using react

--- MyFirstReactApp/src/App.jsx
+++ MyFirstReactApp/src/App.jsx
@@ -1,11 +1,12 @@
+import Cybrom from "./Cybrom"; 
 const AppFirst = () => {
-    return (
-      <div>
-        <h1>Hello My First App</h1>
-        <h2>Hello My second App</h2>
-      </div>
-    );
-  };
-  
-  export default AppFirst;
-  
+  return (
+    <div>
+      <h1>Hello My First App</h1>
+      <h2>Hello My second App</h2>
+      <Cybrom/> {/* Now I am using the Cybrom component */}
+    </div>
+  );
+};
+
+export default AppFirst;  
---
+++ MyFirstReactApp/src/Cybrom.jsx
@@ -0,0 +1,9 @@
+
+const Cybrom=()=>{
+    return(
+        <div>
+            <h1>This is Cybrom New File to connected</h1>
+        </div>
+    )
+}
+export default Cybrom;
