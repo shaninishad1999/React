1844c597231056b02674f58818389d49cde4b1ff
shaninishad1999 <shaninishad1999@gmail.com>
2024-09-17 23:56:50 +0530

    Start React

---
+++ MyFirstReactApp/src/App.jsx
@@ -0,0 +1,11 @@
+const AppFirst = () => {
+    return (
+      <div>
+        <h1>Hello My First App</h1>
+        <h2>Hello My second App</h2>
+      </div>
+    );
+  };
+  
+  export default AppFirst;
+  
