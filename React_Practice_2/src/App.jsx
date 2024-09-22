9c733d563e5a9f704114ace43fb185fae6760d6a
shaninishad1999 <shaninishad1999@gmail.com>
2024-09-22 08:39:21 +0530

    react task done

---
+++ React_Practice_2/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+
+const App=()=>{
+return (
+  <>
+  <div className="head">
+
+  <div className="MainDiv">
+    <div className="SecondDiv">
+        <div className="lastDiv">
+          <h1>Hello Shanideval</h1>
+        </div>
+    </div>
+  </div>
+  </div>
+  </>
+)
+}
+export default App
